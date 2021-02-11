6b53c6bcc98b3d3d45d296a24945b130c59f1332
orcharddu <orcharddu@gmail.com>
2021-02-11 10:53:32 +0800

    fixed history.js

--- js/history.js
+++ js/history.js
@@ -1,25 +1,20 @@
 function History() {
     var history = window.localStorage["history"];
-    if(history == undefined){
-        init();
-    }else{
-        history = JSON.parse(history);
-    }
     var save = function(){
         window.localStorage["history"] = JSON.stringify(history);
     };
-
     var init = function(){
         history = {};
         history.items = [];
         save(history);
     }
-
-    
-
+    if(history == undefined){
+        init();
+    }else{
+        history = JSON.parse(history);
+    }
     this.value = history;
     this.maxLength = 8;
-
     this.add = function(site, owner, repo){
         history.items.push({ "site": site, "owner": owner, "repo": repo });
         if(history.items.length > this.maxLength){
@@ -27,16 +22,13 @@ function History() {
         }
         save();
     };
-
     this.remove = function(index){
         history.items.splice(index, 1);
         save();
     }
-
     this.clear = function(){
         init();
     }
-
     this.index = function(site, owner, repo){
         for(let i = 0; i < history.items.length; i++){
             e = history.items[i];
